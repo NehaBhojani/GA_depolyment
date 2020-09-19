99d7d765b6aa81b6feb55e58da7b9edeb48104ac
Neha Bhojani <neha.ghevaria3@gmail.com>
2020-09-19 15:47:06 +0530

    initial commit

---
+++ bin/dep.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 12:55:39 2020
+
+@author: HP
+"""
+
---
+++ bin/preproccesor.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 12:55:39 2020
+
+@author: HP
+"""
+impot re
+class PreprocessDoc:
+    """
+    Module for preprocessing articles
+    
+    """
+    def removesplchar(text):
+        pass
+    '''
+    Remove special char
+    Input: 
+        text:atring
+    Output:
+        modifiedText: string
+    '''
+    
+    def tokenizeArticle():
+        pass
+    
+    
